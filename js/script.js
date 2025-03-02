96c9fe1bd02a5cbd8eadf66ed91a601dbd4b7f57
L3xc <mail@l3xc.com>
2025-03-02 21:58:55 +0800

    unobstrusively loading javascript

--- js/script.js
+++ js/script.js
@@ -2,37 +2,42 @@
   key: "AIzaSyANsah6p3mH6J62H4jdHcJjjVHRRG0QYGU",
   v: "weekly",
 });
-async function initMap() {
-  const position = { lat: 5.422, lng: 100.295 };
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-  const {ColorScheme} = await google.maps.importLibrary("core")
-  const climbingImg = document.createElement("img");
-  climbingImg.src = "./images/climbing.png"
 
-  map = new Map(document.getElementById("map"), {
-    zoom: 19,
-    center: position,
-    mapId: "6159368a4ef77002",
-    colorScheme: ColorScheme.LIGHT,
-  });
+window.onload = function() 
+{
+  async function initMap() {
+    const position = { lat: 5.422, lng: 100.295 };
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    const {ColorScheme} = await google.maps.importLibrary("core")
+    const climbingImg = document.createElement("img");
+    climbingImg.src = "./images/climbing.png"
 
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    content: climbingImg,
-    title: "Jesseton Rock Wall",
-    gmpClickable: true,
-  });
+    map = new Map(document.getElementById("map"), {
+      zoom: 19,
+      center: position,
+      mapId: "6159368a4ef77002",
+      colorScheme: ColorScheme.LIGHT,
+    });
 
-  map.setMapTypeId('satellite');
-  map.setTilt(60);
+    const marker = new AdvancedMarkerElement({
+      map: map,
+      position: position,
+      content: climbingImg,
+      title: "Jesseton Rock Wall",
+      gmpClickable: true,
+    });
 
-  marker.addListener("gmp-click", ({ domEvent, latLng }) => {
-    const { target } = domEvent;
-    alert("Let's Climb!");
-  });  
+    map.setMapTypeId('satellite');
+    map.setTilt(60);
 
-}
+    marker.addListener("gmp-click", ({ domEvent, latLng }) => {
+      const { target } = domEvent;
+      alert("Let's Climb!");
+    });  
 
-initMap();
+  }
+
+  initMap();
+
+};
