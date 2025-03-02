b99f973e11ee46a7d942a1c9b17f41aa00f8fd65
L3xc <mail@l3xc.com>
2025-03-02 22:05:15 +0800

    fixed js errors

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-  key: "AIzaSyANsah6p3mH6J62H4jdHcJjjVHRRG0QYGU",
-  v: "weekly",
-});
-
 window.onload = function() 
 {
   async function initMap() {
@@ -32,7 +27,6 @@ window.onload = function()
     map.setTilt(60);
 
     marker.addListener("gmp-click", ({ domEvent, latLng }) => {
-      const { target } = domEvent;
       alert("Let's Climb!");
     });  
 
