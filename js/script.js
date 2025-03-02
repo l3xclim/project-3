a6b83ebcbb78080bf59f92444f3bacafe6af4e43
L3xc <mail@l3xc.com>
2025-03-02 21:31:24 +0800

    added google map api

--- js/script.js
+++ js/script.js
@@ -2,27 +2,21 @@
   key: "AIzaSyANsah6p3mH6J62H4jdHcJjjVHRRG0QYGU",
   v: "weekly",
 });
-
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
+  const position = { lat: 5.422, lng: 100.295 };
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 14,
     center: position,
-    mapId: "DEMO_MAP_ID",
+    mapId: "6159368a4ef77002",
   });
 
-  // The marker, positioned at Uluru
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Uluru",
+    title: "Jesseton Rock Wall",
   });
 }
 
